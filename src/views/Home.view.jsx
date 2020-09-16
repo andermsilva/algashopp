4469a70f9b0622773ea9582f67e8156284fe9fd3
andermsilva <67654030+andermsilva@users.noreply.github.com>
2020-09-16 14:28:58 -0300

    clear

---
+++ src/views/Home.view.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomeView(){
+
+    return (
+   
+        <div>
+            todo: home view!!!!!
+        </div>
+    
+    );
+
+
+}
+export default HomeView;
