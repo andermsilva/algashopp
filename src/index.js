3246d8686d4e86f2620bf8e9f4f84fe07d1ae93d
andermsilva <67654030+andermsilva@users.noreply.github.com>
2020-09-29 11:25:21 -0300

    basic-install-redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import HomeView from './views/Home.view'
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeView />
+     <Provider store={store}>
+        <HomeView />
+     </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
