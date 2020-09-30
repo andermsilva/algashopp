e95c4a82682b843b05331b3e6c1c0012d75419e1
andermsilva <67654030+andermsilva@users.noreply.github.com>
2020-09-30 15:15:21 -0300

    stable_1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,93 +13,106 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectSelectedProducts, selectSelectedProductTotalPrice } from '../../store/Products/Products.selectors';
 import toggleProduct from '../../store/Products/Products.action';
 
-function App(){
+function App() {
 
-     const dispatch = useDispatch() ;
-     const colors = ['#62cbc6','#00abac','#006073','#004d61'];
-     //const products = useSelector(selectAllProducts); 
-   
-     const selectedProducts = useSelector(selectSelectedProducts);
-     const totalPrice = useSelector(selectSelectedProductTotalPrice);
-  
-     function handdleToggle(id){
-         dispatch(toggleProduct(id));
-        
-     }
-    return(
-         <Wrapper >                  
-      
-         <Container>
-           <AppHeader/>
-           <AppContainer
-              left={<ShopppingList title='Produtos disponíveis'
-                 onTaggle={handdleToggle}
-               />
+  const dispatch = useDispatch();
+  const colors = ['#62cbc6', '#00abac', '#006073', '#004d61','#9400D3'];
+  //const products = useSelector(selectAllProducts); 
 
-              }
+  const selectedProducts = useSelector(selectSelectedProducts);
+  const totalPrice = useSelector(selectSelectedProductTotalPrice);
 
-              middle={<ShopppingList title='Sua Lista de Compras'
-               onTaggle={handdleToggle}
-               displayOnlySelected
-              />}
-              right={<div>
-                  estatisticas
+  function handdleToggle(id) {
+    dispatch(toggleProduct(id));
 
-                  <LineChart 
-                     color={colors[0]}
-                     title='Saudavel'
-                     percente = {extraPercentage(
-                      selectedProducts.length,
-                      selectedProducts.filter(product=>
-                          product.tags.includes('healthy')).length
-                     )}
-                  />
+  }
+  return (
+    <Wrapper >
 
-                  <LineChart 
-                     color={colors[1]}
-                     title='Não tão Saudavel assim'
-                     percente = {extraPercentage(
-                      selectedProducts.length,
-                      selectedProducts.filter(product=>
-                         product.tags.includes('junk')).length
-                    )}
-                  />
+      <Container>
+        <AppHeader />
+        <AppContainer
+          left={<ShopppingList title='Produtos disponíveis'
+            onTaggle={handdleToggle}
+          />
 
-                   <LineChart 
-                     color={colors[2]}
-                     title='Limpeza'
-                     percente = {extraPercentage(
-                      selectedProducts.length,
-                      selectedProducts.filter(product=>
-                         product.tags.includes('cleaning')).length
-                    )}
-                  />
-                 <LineChart 
-                     color={colors[3]}
-                     title='Outros'
-                     percente = {extraPercentage(
-                      selectedProducts.length,
-                      selectedProducts.filter(product=>
-                         product.tags.includes('others')).length
-                    )}
-                  />
-                <div style={{marginTop:12}}>
-                    <h2 style={{fontWeight:400, fontSize:12, color: '#00364a'}}>
-                        previsão de gastos:
+          }
+
+          middle={<ShopppingList title='Sua Lista de Compras'
+            onTaggle={handdleToggle}
+            displayOnlySelected
+          />}
+          right={<div>
+            estatisticas
+
+                  <LineChart
+              color={colors[0]}
+              title='Saudavel'
+              percente={extraPercentage(
+                selectedProducts.length,
+                selectedProducts.filter(product =>
+                  product.tags.includes('healthy')).length
+              )}
+            />
+
+            <LineChart
+              color={colors[1]}
+              title='Não tão Saudavel assim'
+              percente={extraPercentage(
+                selectedProducts.length,
+                selectedProducts.filter(product =>
+                  product.tags.includes('junk')).length
+              )}
+            />
+
+            <LineChart
+              color={colors[2]}
+              title='Limpeza'
+              percente={extraPercentage(
+                selectedProducts.length,
+                selectedProducts.filter(product =>
+                  product.tags.includes('cleaning')).length
+              )}
+            />
+              <LineChart
+                color={colors[4]}
+                title='Higiene'
+                percente={extraPercentage(
+                  selectedProducts.length,
+                  selectedProducts.filter(product =>
+                    product.tags.includes('hygiene')).length
+                )}
+              />
+            <LineChart
+              color={colors[3]}
+              title='Outros'
+              percente={extraPercentage(
+                selectedProducts.length,
+                selectedProducts.filter(product =>
+                  product.tags.includes('others')).length
+              )}
+            />
+
+
+            <div style={{ marginTop: 12 }}>
+              <h2 style={{ fontWeight: 400, fontSize: 12, color: '#00364a' }}>
+                previsão de gastos:
                      </h2>
-                      <div style={{fontSize:24}}>
-                       {totalPrice.toLocaleString('pt-br',{minimumFractionDigits:2,
-                                                       style: 'currency',currency: 'BRL'})}
-                      </div>
-                    <Calculator/>
-                </div>
-              </div>}
-           
-           />
-         </Container>
+              <div style={{ fontSize: 24 }}>
+                {totalPrice.toLocaleString('pt-br', {
+                  minimumFractionDigits: 2,
+                  style: 'currency', currency: 'BRL'
+                })}
+              </div>
+              <Calculator />
+            </div>
+          </div>}
 
-        </Wrapper>
-    );
+        />
+      </Container>
+
+    </Wrapper>
+  );
 }
 
 export default App;
